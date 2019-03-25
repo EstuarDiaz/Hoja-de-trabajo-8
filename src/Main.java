6a9bbd5a38296e3ea5b56c41b9b087e052e4f441
Estuar Diaz <33070207+EstuarDiaz@users.noreply.github.com>
2019-03-25 16:03:29 -0600

    Segunda implementacion, PriorityQueue

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,51 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.PriorityQueue;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		try {
-			VectorHeap<Paciente> listadoPacientes = new VectorHeap<Paciente>();
 			System.out.println("Ingrese el nombre del archivo de texto que contiene la lista de pacientes.");
 			Scanner input = new Scanner(System.in);
 			File file = new File(input.nextLine());
-			input.close();
 	        Scanner scanner = new Scanner(file);
-	        while (scanner.hasNextLine()){
-	            String line = scanner.nextLine();
-	            String[] data = line.split(",");
-	            Paciente paciente = new Paciente(data[0], data[1], data[2]);
-	            listadoPacientes.add(paciente);
+			System.out.println("Escoga la implemetacion que desea utilizar:\n1. VectorHeap\n2. PriorityQueue\nDefault: PriorityQueue");
+			String respuesta = input.nextLine();
+			input.close();
+	        if(respuesta.equals("1")) {
+	        	/* Programa para la implemetacion con el VectorHeap*/
+				VectorHeap<Paciente> listadoPacientes = new VectorHeap<Paciente>();
+	        	while (scanner.hasNextLine()){
+		            String line = scanner.nextLine();
+		            String[] data = line.split(",");
+		            Paciente paciente = new Paciente(data[0], data[1], data[2]);
+		            listadoPacientes.add(paciente);
+		        }
+		        scanner.close();
+		        System.out.println("Listado de pacientes en orden de prioridad:");
+		        int i = 0;
+		        while(!listadoPacientes.isEmpty()) {
+		        	Paciente paciente = listadoPacientes.remove();
+		        	System.out.println(Integer.toString(++i)+": "+paciente.getNombre()+", "+paciente.getSintoma()+", "+paciente.getPrioridad());
+		        }
 	        }
-	        scanner.close();
-	        System.out.println("Listado de pacientes en orden de prioridad:");
-	        while(!listadoPacientes.isEmpty()) {
-	        	Paciente paciente = listadoPacientes.remove();
-	        	System.out.println(paciente.getNombre()+", "+paciente.getSintoma()+", "+paciente.getPrioridad());
+	        else {
+	        	/* Programa para la implemetacion con JavaUtils, PriorityQueue*/
+				PriorityQueue<Paciente> listadoPacientes = new PriorityQueue<Paciente>();
+	        	while (scanner.hasNextLine()){
+		            String line = scanner.nextLine();
+		            String[] data = line.split(",");
+		            Paciente paciente = new Paciente(data[0], data[1], data[2]);
+		            listadoPacientes.add(paciente);
+		        }
+		        scanner.close();
+		        System.out.println("Listado de pacientes en orden de prioridad:");
+		        int i = 0;
+		        while(!listadoPacientes.isEmpty()) {
+		        	Paciente paciente = listadoPacientes.remove();
+		        	System.out.println(Integer.toString(++i)+": "+paciente.getNombre()+", "+paciente.getSintoma()+", "+paciente.getPrioridad());
+		        }
 	        }
 		}
 		catch(Exception e) {
